63a745b39acabd604d19a5b5f9bc0570689cadf7
Li Guanda <1274805330@qq.com>
2025-02-05 20:51:39 +0800

    WIP: Hit Bounding Box

--- include/bounding_box.h
+++ include/bounding_box.h
@@ -1,17 +1,28 @@
 #ifndef BOUNDING_BOX_H
 #define BOUNDING_BOX_H
 
+#include <cmath>
 #include "interval.h"
 #include "ray.h"
 
 class bounding_box{
   public:
-    interval x, y, z;
+    
+    /// @brief The x, y, z domain 
+    interval x_domain, y_domain, z_domain;
 
-    bounding_box();
+    /// @brief The min and max point for bounding box
+    point min_pt, max_pt;
 
+    /// @brief Construct a bounding box
+    /// @param pt1 The first point, not the min or max point
+    /// @param pt2 The Second Point, not the min or max point
+    bounding_box(point pt1, point pt2);
+
+    /// @brief Is the ray hit the bounding box
+    /// @param r The ray object
+    /// @return true for hit
     bool hit(const ray& r);
-
 };
 
 #endif 
--- src/bounding_box.cpp
+++ src/bounding_box.cpp
@@ -1,1 +1,13 @@
 #include "bounding_box.h"
+
+bounding_box::bounding_box(point pt1, point pt2){
+  this->x_domain = interval(std::min(pt1.x(), pt2.x()), std::max(pt1.x(), pt2.x()));
+  this->y_domain = interval(std::min(pt1.y(), pt2.y()), std::max(pt1.y(), pt2.y()));
+  this->z_domain = interval(std::min(pt1.z(), pt2.z()), std::max(pt1.z(), pt2.z()));
+  this->max_pt = point(this->x_domain.max, this->y_domain.max, this->z_domain.max);
+  this->min_pt = point(this->x_domain.min, this->y_domain.min, this->z_domain.min);
+}
+
+bool bounding_box::hit(const ray& r){
+  return true;
+}
